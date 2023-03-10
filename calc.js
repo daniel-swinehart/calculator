edb794970723f3598bf373f02866b25f151d4dcd
Daniel Swinehart <d4570s@gmail.com>
2023-03-10 16:48:12 +0100

    initial js func, html, and css setup

---
+++ calc.js
@@ -0,0 +1,38 @@
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return num1 + num2;
+};
+
+function subtract(num1, num2) {
+    console.log(num1 - num2);
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    console.log(num1 * num2);
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    console.log(num1 / num2);
+    return num1 / num2;
+};
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            multiply(num1, num2);
+            break;
+    }
+}
+
+
