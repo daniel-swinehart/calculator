f05764a9f7679d0b8ab1fd0e56a2e0248dfcbf7b
Daniel Swinehart <d4570s@gmail.com>
2023-03-14 21:19:32 +0100

    js for 1/0 with snarky comment

--- calc.js
+++ calc.js
@@ -21,13 +21,17 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
     switch(operator) {
         case "+":
-            return add(num1, num2);    
+            return Math.round((add(num1, num2)) * 100) / 100;    
         case "-":
-            return subtract(num1, num2);
+            return Math.round((subtract(num1, num2)) * 100) / 100;
         case "*":
-            return multiply(num1, num2);
+            return Math.round((multiply(num1, num2)) *100) / 100;
         case "/":
-            return divide(num1, num2);
+            if (num2 == 0) {
+                return "You are a DUMMY!";
+            } else {
+            return Math.round((divide(num1, num2)) * 100) / 100;
+            }
     }
 };
 
